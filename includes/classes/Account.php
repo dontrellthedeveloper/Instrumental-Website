7620e5205f5b6f51858c57a63f4595a7bdb4bc06
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-08-26 16:34:07 -0700

    Validation to ensure email and username don't already exist

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -47,6 +47,12 @@ class Account {
             array_push($this->errorArray, Constants::$usernameCharacters);
             return;
         }
+
+        $checkUsernameQuery = mysqli_query($this->con,"SELECT username FROM users WHERE username='$un'");
+        if(mysqli_num_rows($checkUsernameQuery) != 0) {
+            array_push($this->errorArray, Constants::$usernameTaken);
+            return;
+        }
     }
 
     private function validateFirstName($fn) {
@@ -71,6 +77,12 @@ class Account {
 
         if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
             array_push($this->errorArray, Constants::$emailInvalid);
+            return;
+        }
+
+        $checkEmailQuery = mysqli_query($this->con,"SELECT email FROM users WHERE email='$em'");
+        if(mysqli_num_rows($checkEmailQuery) != 0) {
+            array_push($this->errorArray, Constants::$usernameTaken);
             return;
         }
     }
