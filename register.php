8972afdacc33683ab58b0d17a8fb59d15add941f
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-08-25 23:16:46 -0700

    Creating the Register Form

--- register.php
+++ register.php
@@ -12,10 +12,45 @@
         </p>
         <p>
             <label for="loginPassword">Password</label>
-            <input id="loginUPassword" name="loginPassword" type="password" required>
+            <input id="loginPassword" name="loginPassword" type="password" placeholder="e.g. Your password" required>
         </p>
 
         <button type="submit" name="loginButton">LOGIN</button>
+
+    </form>
+
+    <form id="registerForm" action="register.php" method="POST">
+        <h2>Create your free account</h2>
+        <p>
+            <label for="username">Username</label>
+            <input id="username" name="username" type="text" placeholder="e.g. JakeBaker" required>
+        </p>
+        <p>
+            <label for="firstName">First Name</label>
+            <input id="firstName" name="firstName" type="text" placeholder="e.g. Jake" required>
+        </p>
+        <p>
+            <label for="lastName">Last Name</label>
+            <input id="lastName" name="lastName" type="text" placeholder="e.g. Baker" required>
+        </p>
+        <p>
+            <label for="email">Email</label>
+            <input id="email" name="email" type="email" placeholder="e.g. jakebaker@gmail.com" required>
+        </p>
+        <p>
+            <label for="email2">Confirm Email</label>
+            <input id="email2" name="email2" type="email" placeholder="e.g. jakebaker@gmail.com" required>
+        </p>
+        <p>
+            <label for="password">Password</label>
+            <input id="password" name="password" type="password" placeholder="e.g. Your password" required>
+        </p>
+        <p>
+            <label for="password2">Confirm Password</label>
+            <input id="password2" name="password2" type="password" placeholder="e.g. Your password" required>
+        </p>
+
+        <button type="submit" name="registerButton">SIGN UP</button>
 
     </form>
 </div>
