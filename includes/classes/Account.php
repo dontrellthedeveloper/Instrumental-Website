f99b6ad496383f71c220dfb6034ec16ac81c2160
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-08-26 03:16:37 -0700

    Outputting Error Messages

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -14,6 +14,20 @@ class Account {
         $this->validateLastName($ln);
         $this->validateEmails($em, $em2);
         $this->validatePasswords($pw, $pw2);
+
+        if(empty($this->errorArray)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public function getError($error) {
+        if(!in_array($error, $this->errorArray)) {
+            $error = "";
+        }
+        return "<span class='errorMessage'>$error</span>";
     }
 
     private function validateUsername($un) {
