af02cf6028af77a3f6a2c88bffd524d01a357e2b
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-08-28 03:40:48 -0700

    Adding Navigation Items

--- index.php
+++ index.php
@@ -25,9 +25,35 @@ if (isset($_SESSION['userLoggedIn'])) {
 
             <div id="navBarContainer">
                 <nav class="navBar">
+
                     <a href="index.php" class="logo">
                         <img src="assets/images/icons/logo.png" alt="">
                     </a>
+
+                    <div class="group">
+
+                        <div class="navItem">
+                            <a href="search.php" class="navItemLink">Search</a>
+                        </div>
+
+                    </div>
+
+                    <div class="group">
+
+                        <div class="navItem">
+                            <a href="browse.php" class="navItemLink">Browse</a>
+                        </div>
+
+                        <div class="navItem">
+                            <a href="yourMusic.php" class="navItemLink">Your Music</a>
+                        </div>
+
+                        <div class="navItem">
+                            <a href="profile.php" class="navItemLink">Dontrell Washington</a>
+                        </div>
+
+                    </div>
+
                 </nav>
             </div>
 
