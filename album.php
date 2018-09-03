ce5a0e4525fc86678410128817ed2398de2ea0e9
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-09-03 00:42:35 -0700

    Created Album Header Section

--- album.php
+++ album.php
@@ -14,8 +14,20 @@ $artist = $album->getArtist();
 
 echo $album->getTitle() . "<br>";
 echo $artist->getName();
-
 ?>
+
+<div class="entityInfo">
+
+    <div class="leftSection">
+        <img src="<?php echo $album->getArtworkPath();?>" alt="">
+    </div>
+
+    <div class="rightSection">
+        <h2><?php echo $album->getTitle();?></h2>
+        <span>By <?php echo $artist->getName()?></span>
+    </div>
+
+</div>
 
 
 
