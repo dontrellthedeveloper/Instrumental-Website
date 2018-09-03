d2ab3e7e5fa7e95e4a2575e3a366ebc2e9a57e66
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-09-03 15:37:43 -0700

    Format the Time Remaining

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,31 @@
 let currentPlaylist = [];
 let audioElement;
 
+
+function formatTime(second) {
+    let time = Math.round(second);
+    let minutes = Math.floor(time / 60);
+    let seconds = time - (minutes * 60);
+
+    let extraZero;
+
+    if(seconds < 10) {
+        extraZero = "0";
+    } else  {
+        extraZero = "";
+    }
+
+    return minutes + ":" + extraZero + seconds;s
+}
+
 function Audio() {
 
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
 
     this.audio.addEventListener("canplay", function () {
-        $(".progressTime.remaining").text(this.duration);
+        let duration = formatTime(this.duration)
+        $(".progressTime.remaining").text(duration);
     })
 
     this.setTrack = function (track) {
