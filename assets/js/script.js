8f5bc5b4f2287544a2155a59c1c42f43576bf71c
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-09-03 20:31:31 -0700

    Change URL when we Load a New Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,8 @@ function openPage(url) {
     }
     let encodedUrl = encodeURI(url + "&userLoggedIn=" + userLoggedIn);
     $("#mainContent").load(encodedUrl);
+    $("body").scrollTop(0);
+    history.pushState(null, null, url);
 }
 
 function formatTime(second) {
