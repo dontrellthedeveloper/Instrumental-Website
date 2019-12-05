388e470d9847ff4703b68a9d6cb148a8247ce277
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-12-05 13:45:09 -0800

    Added db config

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,41 @@
 <?php
-    ob_start();
-    session_start();
+//    ob_start();
+//    session_start();
+//
+//    $timezone = date_default_timezone_set("America/Los_Angeles");
+//
+//    $con = mysqli_connect("localhost", "root", "", "talentwise");
+//
+//    if(mysqli_connect_errno()) {
+//        echo "Failed to connect: ". mysqli_connect_errno();
+//    }
+//?>
 
-    $timezone = date_default_timezone_set("America/Los_Angeles");
 
-    $con = mysqli_connect("localhost", "root", "", "talentwise");
+<?php
+$active_group = 'default';
+$query_builder = TRUE;
 
-    if(mysqli_connect_errno()) {
-        echo "Failed to connect: ". mysqli_connect_errno();
-    }
+$con['default'] = array(
+    'dsn' => '',
+    'hostname' => 'us-cdbr-iron-east-05.cleardb.net',
+    'username' => 'ba4059fc3d38bf',
+    'password' => 'd6f95111',
+    'database' => 'heroku_c58f954269ec2f0',
+    'dbdriver' => 'mysqi',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+//    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE,
+)
+
 ?>
